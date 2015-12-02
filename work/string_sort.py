c7c3c03d41851d5825e24e943ba4fc3f53e703c0
Raghavendra Gamoji <rgamoji@expedia.com>
2015-12-01 16:51:16 -0800

    Checking in after a long time.

--- work/string_sort.py
+++ work/string_sort.py
@@ -5,7 +5,7 @@ def main():
    string=raw_input("Enter a String: ")
    sort_me=Sort()
    n_list=sort_me.convert_string_to_ascii(string)
-   sort_me.bubble_sort(n_list)
+   sort_me.bubble_sort(n_list,True)
    print sort_me.convert_ascii_to_string(n_list)
    original=[]
    for n in range(0,10):
