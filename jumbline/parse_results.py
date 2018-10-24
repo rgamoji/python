64a2b2db60cd1dded9e81971f7095604453b8f34
Raghavendra Gamoji <rgamoji@gmail.com>
2018-10-24 11:22:34 -0700

    .

--- jumbline/parse_results.py
+++ jumbline/parse_results.py
@@ -4,13 +4,14 @@ import json
 import ast
 # I had to implement this silly hack to clean-up and format the results into a proper json structure.
 # This one helped: https://stackoverflow.com/questions/25707558/json-valueerror-expecting-property-name-line-1-column-2-char-1
-target=[]
-with open(".rgamoji.jmg", "r") as fH:
+target={
+   "Games":[]
+}
+
+with open(".rgamoji.jmg.orig", "r") as fH:
     for line in fH.readlines():
       json_input=ast.literal_eval(line)
-      target.append(json_input) 
-
-with open(".rgamoji.img.bkp","w") as fW:
-   for rec in target:
-      fW.write(json.dumps(rec,indent=2))
-      fW.write("\n")
+      target["Games"].append(json_input) 
+with open(".rgamoji.jmg.bkp","w") as fW:
+    fW.write(json.dumps(target,indent=2))
+    fW.write("\n")
